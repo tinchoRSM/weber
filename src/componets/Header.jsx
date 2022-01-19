60fe4b9aa59eb6a8bd8e9c031225eb7e7b1ee2e5
Radostin Minchev <tinchoRSM@gmail.com>
2022-01-19 11:13:39 +0200

    FIxed responsivness.

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -3,19 +3,19 @@ import { Link }from 'react-router-dom';
 
 import {ReactComponent as LogoIcon} from '../img/svg/weber-logo.svg'
 import {ReactComponent as HomeIcon} from '../img/svg/home-svgrepo-com.svg'
-import {ReactComponent as CartIcon} from '../img/svg/cart-4-svgrepo-com.svg'
+
  
 
 function Header() {
     return (
         <Navigation>    
-            <NavItem destination="/weber/" icon={<LogoIcon className="h-[36px] w-[78px]"/>} text=""/>
-            <NavItem destination="/weber/" icon="" text="Начало"/>
-            <NavItem destination="/weber/forus" icon="" text="За нас" />
-            <NavItem destination="/weber/clients" icon="" text="Обслужване на клиенти" />
-            <NavItem destination="/weber/location" icon="" text="Магазини" />
+            <NavItem destination="/" icon={<LogoIcon className="h-[36px] w-[78px]"/>} text=""/>
+            <NavItem destination="/" icon="" text="Начало"/>
+            <NavItem destination="/forus" icon="" text="За нас" />
+            <NavItem destination="/clients" icon="" text="Обслужване на клиенти" />
+            <NavItem destination="/location" icon="" text="Магазини" />
             <li className="flex center justify-center items-center px-4 h-full hover:bg-red-700">
-                <a href="https://bradva.net/weber-barbecue/"><CartIcon/></a>
+                <a href="https://bradva.net/weber-barbecue/">   </a>
             </li>
             
 
@@ -37,7 +37,7 @@ function Navigation(props) {
 
 function NavItem(props) {
     return(
-        <li className="flex center justify-center items-center px-4 h-full hover:bg-red-700">
+        <li className="flex justify-center items-center px-4 h-full hover:bg-red-700">
             <Link to={props.destination} className="flex">
                 <span className="" >{props.icon}</span>
                 <span>{props.text}</span>
